ce45d4bdae959a191014f5575cfb26850e0a7f61
ahills1 <70347865+ahills1@users.noreply.github.com>
2020-09-07 12:42:26 -0400

    Add files via upload

---
+++ Workbook Assignment 1b.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+Location = "/Users/ashle/OneDrive/Documents/USF/ISM 4402- Business Intelligence/datasets/datasets/CRM01.xls"
+df = pd.read_excel(Location)
+
+
+# In[2]:
+
+
+df.head()
+
+
+# In[ ]:
+
+
+
+
