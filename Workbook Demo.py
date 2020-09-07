493a2514cd9397ec3677d426cb8a1e54b2298ab2
ahills1 <70347865+ahills1@users.noreply.github.com>
2020-09-07 11:17:20 -0400

    Add files via upload

---
+++ Workbook Demo.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+Location = "/Users/ashle/OneDrive/Documents/USF/ISM 4402- Business Intelligence/datasets/datasets/smallgradesh.csv"
+df = pd.read_csv(Location, header=None)
+
+
+# In[2]:
+
+
+df.head()
+
+
+# In[ ]:
+
+
+
+
